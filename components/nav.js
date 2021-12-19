564a2cd3b0832f9861fb507fad3c8a867c1ada3d
Balao <matheus.balao@hotmail.com>
2021-12-19 18:05:59 -0300

    Fix navbar links

--- components/nav.js
+++ components/nav.js
@@ -1,5 +1,5 @@
 document.getElementById("navMenu").innerHTML =
-    "<a href='/index.html' class='btn btn-link'><span class='mr-1'><img src='./resources/flags/us.svg' alt='us' width='20px'></span>English</a>" +
+    "<a href='./index.html' class='btn btn-link'><span class='mr-1'><img src='./resources/flags/us.svg' alt='us' width='20px'></span>English</a>" +
     "<a href='./pt-BR/index.html' class='btn btn-link'><span class='mr-1'><img src='./resources/flags/br.svg' alt='pt-BR' width='20px'></span>Português</a>" +
     "<a href='./de/index.html' class='btn btn-link'><span class='mr-1'><img src='./resources/flags/de.svg' alt='de' width='20px'></span>Deutsch</a>";
 
