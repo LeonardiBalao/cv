ff3321b12fa5f70c9574694725bb45de1ddeeffa
Balao <matheus.balao@hotmail.com>
2021-12-19 13:13:37 -0300

    Finish Education

---
+++ components/footer.js
@@ -0,0 +1,5 @@
+document.getElementById("footer").innerHTML =
+    "<a href='/index.html' class='btn btn-link'><span class='mr-1'><img src='./resources/flags/us.svg' alt='de' width='20px'></span>English</a>" +
+    "<a href='./pt-BR/index.html' class='btn btn-link'><span class='mr-1'><img src='./resources/flags/br.svg' alt='de' width='20px'></span>Português</a>" +
+    "<a href='./de/index.html' class='btn btn-link'><span class='mr-1'><img src='./resources/flags/de.svg' alt='de' width='20px'></span>Deutsch</a>";
+
